5aa9493b1175cafcfbcd6440320b60d3f9f28e49
mbejadiana <di.mbeja@gmail.com>
2022-03-11 11:42:50 -0800

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - print the letters of the alphabet 
- *
- * Description: print the letters of the alphabet except e, q
- *
+ * main - print the letters of the alphabet
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i = 97:
+	int i = 97;
 
 	while (i < 123)
 	{
 		if (i != 101 && i != 113)
-	{
-		putchar(i);
-
-
+		{
+			putchar(i);
+		}
+		i++;
+	}
 	putchar(10);
-
 	return (0);
 }
