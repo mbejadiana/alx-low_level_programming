d12c8a1f02c73f0cf305ef5153bfc79346d8ec7c
mbejadiana <di.mbeja@gmail.com>
2022-03-11 10:05:28 -0800

    invention

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+  * main - print single digit numbers
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 57 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
