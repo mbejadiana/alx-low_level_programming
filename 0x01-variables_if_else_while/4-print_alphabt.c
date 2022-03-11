468c196ebeba9620d8318e334af8453315980e4d
mbejadiana <di.mbeja@gmail.com>
2022-03-11 06:51:59 -0800

    numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,18 +10,16 @@
 
 int main(void)
 {
-	int i = 97;
+	int i = 97:
 
 	while (i < 123)
 	{
 		if (i != 101 && i != 113)
 	{
 		putchar(i);
-	}
-	i++;
 
-	}
-	putchar (10);
+
+	putchar(10);
 
 	return (0);
 }
