76892de4afac8d8f0cf14b279e06152fa3c074fd
mbejadiana <di.mbeja@gmail.com>
2022-03-14 04:34:25 -0700

    size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
-*main - Entry point
-*
-* Description: prints "Programming is like building a multilingual puzzle"
-*
-* Return: Always 0 (Success)
-*/
+  * main - programming is a multilingual puzzle
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - print the size of various types
- *
- * Description: prints the size of various types on the computer it is compiled 
- * and run on 
- *
+ * main - entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", (int) sizeof(float);
-	
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
 	return (0);
-}	
+}
